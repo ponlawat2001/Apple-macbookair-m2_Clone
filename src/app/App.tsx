092ce40796264b16acb6b936bdf372b5245cf642
mix23456 <mixsax2@gmail.com>
2023-09-12 23:52:44 +0700

    buttonwithhover

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "../routers/router";
+import Example from "../nav/nav";
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <Router></Router>
+        <Example />
+        <Router />
       </BrowserRouter>
     </React.StrictMode>
   );
--- src/view/home/home.tsx
+++ src/view/home/home.tsx
@@ -1,10 +1,20 @@
-import Example from "../../nav/nav";
-
+import "./home.css";
 function HomeScreen() {
   return (
     <>
-      <div className="text-3xl font-bold underline">Hello world</div>
-      <Example></Example>
+      <div className="p-4">
+        <button
+          className="
+      container py-4 
+      rounded-2xl 
+      bg-blue-500 
+      active:bg-blue-600 
+      hover:bg-white 
+      hover:text-cyan-950"
+        >
+          <div>Hello world</div>
+        </button>
+      </div>
     </>
   );
 }
